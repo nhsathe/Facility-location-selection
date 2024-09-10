b2130c4100277fc136e258b7dbe49d7c4406d81f
nhsathe <112503941+nhsathe@users.noreply.github.com>
2024-09-10 11:34:26 -0400

    Update projectcode_obj1.py

--- projectcode_obj1.py
+++ projectcode_obj1.py
@@ -72,9 +72,15 @@ print(df)
 model= ConcreteModel(name = "Support center optimization")
 model.I=Set(initialize=range(1,19))
 model.J= Set(initialize=range(1,19))
-P=int(input("Enter number of support centers to be built:"))
+#P=int(input("Enter number of support centers to be built:"))
 
+import streamlit as st
 
+P = st.number_input("Enter number of support centers to be built:", 
+                             min_value=0,  # No minimum value
+                             max_value=18,  # No maximum value
+                             value=0,  # Default value
+                             step=1) 
 
 
 

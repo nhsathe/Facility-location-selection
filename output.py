e38e0c058fc6c1eebde2e558ec47ae5508b30e89
nhsathe <112503941+nhsathe@users.noreply.github.com>
2024-09-13 15:36:18 -0400

    Update output.py

--- output.py
+++ output.py
@@ -24,7 +24,7 @@ def main():
     P = st.slider("Select number of support centers to be built", min_value=1, max_value=18, value=st.session_state.P)
     st.session_state.P = P  # Update session state
 
-    if st.button("Run Model"):
+    if st.button("Run Model"):  
         # Import the appropriate model based on user selection
         if o == "P-Median":
             from output_obj1 import model, Zipcode1
